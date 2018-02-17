ad6daf0750f8c7807d8399d5e09d77297e72ede4
Christopher Henn <chris@chrishenn.net>
2018-02-16 16:59:16 -0800

    Extract SQL query as constants

--- ucsc_gene_search.py
+++ ucsc_gene_search.py
@@ -9,9 +9,19 @@ from pymysql import connect
 from pymysql.cursors import DictCursor
 from pyfaidx import Fasta
 
+
 CHROMOSOME_DATA_URL = "ftp://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/{}.fa.gz"
 CHROMOSOME_DATA_DIR = "chromosome_data"
 CHROMOSOME_DATA_FILE = "{}.fa"
+
+GENE_BY_ID_SQL = "SELECT * FROM knownGene WHERE name = '{}'"
+
+GENE_SEARCH_SQL = ("SELECT ncbiRefSeqLink.name, ncbiRefSeqLink.product, kgAlias.kgID "
+                   "FROM ncbiRefSeqLink "
+                   "JOIN kgAlias "
+                   "ON SUBSTRING(ncbiRefSeqLink.id, 1, LENGTH(ncbiRefSeqLink.id) - 2) = kgAlias.alias "
+                   "WHERE ncbiRefSeqLink.name LIKE '{}'")
+
 
 connection = connect(host='genome-mysql.soe.ucsc.edu',
                      user='genome',
@@ -22,13 +32,7 @@ connection = connect(host='genome-mysql.soe.ucsc.edu',
 
 def get_search_results(search_term):
     with connection.cursor() as cursor:
-        query = ("SELECT ncbiRefSeqLink.name, ncbiRefSeqLink.product, kgAlias.kgID "
-                 "FROM ncbiRefSeqLink "
-                 "JOIN kgAlias "
-                 "ON SUBSTRING(ncbiRefSeqLink.id, 1, LENGTH(ncbiRefSeqLink.id) - 2) = kgAlias.alias "
-                 "WHERE ncbiRefSeqLink.name LIKE '{}'")
-
-        cursor.execute(query.format(search_term))
+        cursor.execute(GENE_SEARCH_SQL.format(search_term))
         results = cursor.fetchmany(5)
 
         return results
@@ -53,9 +57,7 @@ def get_search_result_selection(results):
 
 def get_gene_data(selection):
     with connection.cursor() as cursor:
-        query = "SELECT * FROM knownGene WHERE name = '{}'"
-
-        cursor.execute(query.format(selection['kgID']))
+        cursor.execute(GENE_BY_ID_SQL.format(selection['kgID']))
         gene_data = cursor.fetchone()
 
         return gene_data

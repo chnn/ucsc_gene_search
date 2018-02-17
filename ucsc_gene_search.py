48d98b713c300113fe6a71e55e22ecf5fa5685dd
Christopher Henn <chris@chrishenn.net>
2018-02-16 16:55:00 -0800

    Add ability to download sequence data

--- ucsc_gene_search.py
+++ ucsc_gene_search.py
@@ -1,7 +1,17 @@
+import os
 import sys
+import gzip
+import urllib.request
+
+from pathlib import Path
 
 from pymysql import connect
 from pymysql.cursors import DictCursor
+from pyfaidx import Fasta
+
+CHROMOSOME_DATA_URL = "ftp://hgdownload.soe.ucsc.edu/goldenPath/hg38/chromosomes/{}.fa.gz"
+CHROMOSOME_DATA_DIR = "chromosome_data"
+CHROMOSOME_DATA_FILE = "{}.fa"
 
 connection = connect(host='genome-mysql.soe.ucsc.edu',
                      user='genome',
@@ -51,6 +61,44 @@ def get_gene_data(selection):
         return gene_data
 
 
+def path_for_chromosome_data(chromosome_key):
+    return Path(CHROMOSOME_DATA_DIR + "/" + CHROMOSOME_DATA_FILE.format(chromosome_key))
+
+
+def url_for_chromosome_data(chromosome_key):
+    return CHROMOSOME_DATA_URL.format(chromosome_key)
+
+
+def get_chromosome_data(chromosome_key):
+    out_path = path_for_chromosome_data(chromosome_key)
+
+    if out_path.exists():
+        # Data has already been downloaded
+        return
+
+    # Download FASTA file from UCSC Genome FTP server
+    response = urllib.request.urlopen(url_for_chromosome_data(chromosome_key))
+
+    # Make the data directory if it doesn't already exist
+    os.makedirs(CHROMOSOME_DATA_DIR, exist_ok=True)
+
+    # Decompress the FASTA file into the data directory
+    with open(out_path, 'wb') as out_file:
+        out_file.write(gzip.decompress(response.read()))
+
+
+def extract_chromosome_data(chromosome_key, start, end):
+    data_path = path_for_chromosome_data(chromosome_key)
+
+    if not data_path.exists():
+        raise Exception("Chromosome data not downloaded")
+
+    all_data = Fasta(str(data_path))
+    sliced_data = all_data[chromosome_key][start:end].seq
+
+    return sliced_data
+
+
 if __name__ == '__main__':
     try:
         search_term = input("Enter a search term: ")
@@ -63,7 +111,16 @@ if __name__ == '__main__':
         selection = get_search_result_selection(search_results)
         gene_data = get_gene_data(selection)
 
+        print("Gene data:\n")
+
         for k, v in gene_data.items():
             print("{}: {}".format(k, v))
+
+        print("\nDownloading sequence data...")
+
+        get_chromosome_data(gene_data['chrom'])
+
+        print("Finished. Sequence data:\n")
+        print(extract_chromosome_data(gene_data['chrom'], gene_data['txStart'], gene_data['txEnd']))
     finally:
         connection.close()
